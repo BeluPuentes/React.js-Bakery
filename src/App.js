a17c522a0b2acf1a691526830b1e3ad1c0d5dd84
BeluPuentes <belu.puentes@hotmail.com>
2022-03-01 14:55:31 -0300

    NavBar

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route, useRoutes } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import Comprar from './pages/comprar';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+
+function App() {
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route path='/' exact component={<Home/>} />
+		<Route path='/about' component={<About/>} />
+		<Route path='/events' component={<Events/>}/>
+		<Route path='/comprar' component={<Comprar/>} />
+		<Route path='/team' component={<Teams/>} />
+		<Route path='/blogs' component={<Blogs/>} />
+		<Route path='/sign-up' component={<SignUp/>} />
+	</Routes>
+	</Router>
+);
+}
+
+
+export default App;
