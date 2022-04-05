14c7592fec6f569fb3ad1c9413b702139666b79d
BeluPuentes <belu.puentes@hotmail.com>
2022-04-05 09:32:09 -0300

    optimizando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar';
-import { BrowserRouter as Router, Routes, Route, useRoutes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
 import Events from './pages/events';
-import Comprar from './pages/comprar';
 import Teams from './pages/team';
 import Blogs from './pages/blogs';
 import SignUp from './pages/signup';
-import Titulo from './components/Titulo';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
 import Modal from './Modal/Modal';
 import {CartProvider} from './Context/CartContext'
 import {Cart} from './components/Cart/Cart';
 import { Checkout } from './components/Checkout/Checkout';
-
-alert ("Bienvenido");
 
 
 
